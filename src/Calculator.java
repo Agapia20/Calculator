61c40a84b7c037c3cbf88b4f748dc34a9dedfd36
BermetAidarova <aidarovab@gmail.com>
2019-01-21 18:09:03 -0600

    add subtrack method

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,5 +3,9 @@ public class Calculator {
         public static double  add(double num1, double num2){
             return num1 + num2;
         }
+        public static double  subtrct(double num1, double num2){
+            return num1 - num2;
+        }
+
 
 }
